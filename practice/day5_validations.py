4bf70c7625f096cd559e6ddb1e2e570f636d74ef
Ankita Thombre <thombreankita29@gmail.com>
2026-06-06 16:59:21 +0530

    Day 5 — required_columns validation with list comprehension and ValueError

---
+++ practice/day5_validations.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from pathlib import Path
+
+def required_columns(df: pd.DataFrame, required: list) -> None:
+    missed =[col for col in required if col not in df.columns]
+    if missed :
+        raise ValueError(f"Missing Columns:{missed}")
+
+
+filep = (Path(__file__).resolve().parent.parent / "data" / "raw" / "PS_20174392719_1491204439457_log.csv")
+df = pd.read_csv(filep, nrows=100000)
+required = df.columns.to_list()
+print(required)
+req_col = ['amount', 'nameOrig', 'oldbalanceOrg', 'newbalanceOrig','isFraud', 'isFlaggedFraud']
+required_columns(df,req_col)
