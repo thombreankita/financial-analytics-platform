663bf9b19ec73215f9237fe063978aee7de1b1a7
Ankita Thombre <thombreankita29@gmail.com>
2026-06-06 12:14:25 +0530

    Additional methods to understand the data of PaySim dataset!

--- practice/day4_pandas.py
+++ practice/day4_pandas.py
@@ -9,6 +9,7 @@ def load_and_inspect(filepath: str) -> pd.DataFrame:
     print(df.dtypes)#gives the datatype of all the columns
     print('Nulls:' ) #isnull() gives theentire df in matrix form of true and false values 
     print(df.isnull().sum()); #the sum() adds the total number of true in a particular column and gives the sum column wise
+    print(df['type'].unique())
     return df
 
 
@@ -18,8 +19,13 @@ def get_positive_transactions(df: pd.DataFrame) -> pd.DataFrame:
 
         raise ValueError("amount column missing")
     print(df[df["amount"] > 0])
+    print("Positive transactions:",len(df[df["amount"]>0]))
     return df[df["amount"] > 0] # df["amount"] > 0 creates a list 
 #fpath = Path("/d/Financial-analytics-platform/data/raw/PS_20174392719_1491204439457_log.csv")
+
+def get_fraud_transaction(df : pd.DataFrame) -> pd.DataFrame:
+    return df[df['isFraud'] == 1]
+
 
 fpath = ( Path(__file__).resolve().parent.parent /"data"/"raw"/"PS_20174392719_1491204439457_log.csv")
 df = load_and_inspect(fpath)
@@ -27,3 +33,10 @@ df = load_and_inspect(fpath)
 positive_df = get_positive_transactions(df)
 
 print(positive_df.head())
+print(positive_df.shape)
+
+fraud_df = get_fraud_transaction(df)
+
+print(fraud_df.to_string())
+print(fraud_df.shape)
+print(fraud_df['type'].unique())
