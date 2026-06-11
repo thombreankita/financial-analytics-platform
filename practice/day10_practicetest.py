c320fb03d98ebc90f336b5309d17a41d77f76743
Ankita Thombre <thombreankita29@gmail.com>
2026-06-11 14:49:10 +0530

    entry test check_file_ready complete, done

--- practice/day10_practicetest.py
+++ practice/day10_practicetest.py
@@ -4,15 +4,14 @@ from pathlib import Path
 def check_file_ready(fp: Path) -> bool:
    if not fp.exists():
       raise FileNotFoundError("File Not Found!!")
-   if len(fp.read_bytes()) == 0:
+   if fp.suffix != ".csv":
+      raise ValueError("This is not a .csv file!!")
+   if fp.stat().st_size == 0:
       raise ValueError("Empty File")
    return True
 
 filep = Path(__file__).parent.parent / "data" / "processed" / "transaction_CASH_OUT.csv"
 try:
-   if filep.suffix != ".csv":
-      raise ValueError("This is not a .csv file!!")
-   else:
-      print(check_file_ready(filep));
+   print(check_file_ready(filep))
 except (ValueError,FileNotFoundError) as e:
    print(f'Error: {e}')
