8176935ebb0c60bc882cccef767804115daef45d
Ankita Thombre <thombreankita29@gmail.com>
2026-07-14 00:08:11 +0530

    Day 22 - Py Spark transformations

--- transformation/spark_transform.py
+++ transformation/spark_transform.py
@@ -1,13 +1,26 @@
 from pathlib import Path
 from pyspark.sql import DataFrame
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, IntegerType, DoubleType, StringType
 
 def create_s_session(app_name: str ) -> SparkSession:
     spark = (SparkSession.builder.appName(app_name).master("local[*]").getOrCreate())
     return spark
 
 def load_data_spark(spark: SparkSession, filepath: str) -> DataFrame:
-    df = spark.read.csv(filepath, header = "true", inferSchema="true")
+    schema = StructType([
+    StructField("step", IntegerType(), True),
+    StructField("type", StringType(), True),
+    StructField("amount", DoubleType(), True),
+    StructField("nameOrig", StringType(), True),
+    StructField("oldbalanceOrg", DoubleType(), True),
+    StructField("newbalanceOrig", DoubleType(), True),
+    StructField("nameDest", StringType(), True),
+    StructField("isFraud", IntegerType(), True),
+    StructField("isFlaggedFraud", IntegerType(), True),
+    ])
+    df = spark.read.csv(filepath, header=True, schema=schema)
+    #df = spark.read.csv(filepath, header = True, inferSchema="true")
     df.printSchema()
     df.show(5)
     print(f'Row Count: {df.count()}')
