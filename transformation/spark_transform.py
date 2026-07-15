9016cce961952618bbf077c102f46749d663d311
Ankita Thombre <thombreankita29@gmail.com>
2026-07-15 23:25:57 +0530

    Added the complete volume function

--- transformation/spark_transform.py
+++ transformation/spark_transform.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from pyspark.sql import DataFrame
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, IntegerType, DoubleType, StringType
+from pyspark.sql import functions as F
 
 def create_s_session(app_name: str ) -> SparkSession:
     spark = (SparkSession.builder.appName(app_name).master("local[*]").getOrCreate())
@@ -28,10 +29,21 @@ def load_data_spark(spark: SparkSession, filepath: str) -> DataFrame:
     print(f'Row Count: {df.count()}')
     return df
 
+def calculate_daily_transaction_volume(df: DataFrame) -> DataFrame:
+    df_r = df.groupBy('step','type').agg(
+        F.count('*').alias('transaction_count'),
+        F.sum('amount').alias('total_amount'),
+        F.avg('amount').alias('avg_amount')
+    ).orderBy('step')
+    return df_r
+
 def main():
     fpath = str(Path(__file__).parent.parent / "data" /"raw"/"PS_20174392719_1491204439457_log.csv")
     sp_sess = create_s_session("FinancialAnalytics")
     df = load_data_spark(sp_sess,fpath)
+    volumndf = calculate_daily_transaction_volume(df)
+    volumndf.show(20)
+    print(f'Volumn aggregation: {volumndf.count()}')
     sp_sess.stop()
 
  
