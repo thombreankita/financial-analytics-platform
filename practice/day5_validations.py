1d7d6c6477fb7d239462af8cce1badd4faec1654
Ankita Thombre <thombreankita29@gmail.com>
2026-09-25 23:49:31 +0530

    Practice

--- practice/day5_validations.py
+++ practice/day5_validations.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pathlib import Path
 
 def required_columns(df: pd.DataFrame, required: list) -> None:
-    missed =[col for col in required if col not in df.columns]
+    missed =[col for col in required if col not in df.columns]  # List Comprehension
     if missed :
         raise ValueError(f"Missing Columns:{missed}")
 
